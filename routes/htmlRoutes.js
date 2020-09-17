ece002d355b2b9cb71b666664cd77cb97cc19cf1
Scott Zinski <zoolunk@gmail.com>
2020-09-17 14:41:54 -0400

    fix handlebars issues and image scraping

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,9 +4,9 @@ var router = express.Router()
 var db = require("../models");
 
   // Route for getting all Articles from the db
-  
+
 router.get("/favorites", function(req, res) {//get favorite articles
-  db.Article.find({'favorite': true}).then(function(articles){
+  db.Article.find({'favorite': true}).lean().then(function(articles){
     res.render("saved", {articles: articles, title: "Saved", saved: "true", titleCard: "Save This"});
   })
   .catch(function(err) {
@@ -15,14 +15,14 @@ router.get("/favorites", function(req, res) {//get favorite articles
 });
 
 router.get("/", function(req, res) {//get all articles
-  db.Article.find({}).then(function(articles){
+  db.Article.find({}).lean().then(function(articles){
     res.render("landing", {articles: articles, title: "Scape This!", home:"true", titleCard: "Scrape This"});
   })
   .catch(function(err) {
     res.json(err);
   });
 });//end of all articles
-  
+
 router.get('*', function(req, res){//404 not available
 res.status(404).send("404 Not Available")
 })
--- routes/scrapeRoutes.js
+++ routes/scrapeRoutes.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res) {// router to scrape singletrack web page
   db.Article.find({}, "tag")
   .then(function(tags){
     let articleTags = tags.map(data => data.tag);
-    
+
     axios.get("https://www.singletracks.com/").then(function(response) {
       var $ = cheerio.load(response.data);
       let results = [];
@@ -19,8 +19,8 @@ router.get("/", function(req, res) {// router to scrape singletrack web page
         result.title = $(this).find("h2 a").text();
         result.tag = $(this).find("h2 a").attr("href");
         result.text = $(this).find(".nf_preview_text").text().trim();
-        result.image = $(this).find(".col-md-5").css("background-image").replace('url(\'','').replace('\')','').replace(/\"/gi, "")
-        console.log("img: " , result.image);
+        // result.image = $(this).find(".col-md-5").css("background-image").replace('url(\'','').replace('\')','').replace(/\"/gi, "")
+        // console.log("img: " , result.image);
         // results.push(result);
         if (!articleTags.includes(result.tag)){
           results.push(result);
